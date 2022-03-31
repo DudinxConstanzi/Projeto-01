5c6eb2f7a3a303ee40c18cdbb05984b55560f07f
DudinxConztanzi <memartins161@gmail.com>
2022-03-31 14:58:29 -0300

    upload projeto

---
+++ Workshop Git/Shared/Workshop_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workshop_GitApp.swift
+//  Shared
+//
+//  Created by Dudinx Martins on 31/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Workshop_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
